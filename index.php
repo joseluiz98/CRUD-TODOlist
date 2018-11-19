875611615e458766a15de0c90b3f948f080feab6
José Luiz <joseluiz98.jl@gmail.com>
2018-11-19 18:16:15 -0200

    Edit image and edit function has some code

--- index.php
+++ index.php
@@ -41,6 +41,9 @@
 					echo nl2br($task['descricao']);
 					echo "</div>";
 					echo '<div class="col-sm-2">';
+					echo "<a data-toggle='modal' data-target='#editTaskModal'>";
+					echo "<img src='imgs/edit.svg'/>";
+					echo "</a>";
 					echo "<a href='delete.php?id=" .$task['id']."'>";
 					echo "<img src='imgs/del.svg'/>";
 					echo "</a>";
@@ -51,6 +54,25 @@
 			?>
 		</div>
 
+		<div class="modal" tabindex="-1" role="dialog" id="editTaskModal">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title">Modal title</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<p>Modal body text goes here.</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+			</div>
+		</div>
+		</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
